86896e1d6627dd685e6facb2691b18c71d3050e1
adityabhosle <adityabhsl83@gmail.com>
2015-07-05 21:36:43 +0530

    Added js for homepage

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	$('body').scrollspy({ target: '.navbar' }); /*To highlight current position on the navbar while scrolling*/
+
+	$('#form').on('submit', function(ev) {
+
+		// prevent from auto-submitting  
+		ev.preventDefault();
+		ev.stopPropagation();
+
+		// if all is good, then submit.
+		sendData();
+
+	});
+
+    $(".nav li a").on("click", function(){
+		// so that the clicked one gets highlighted
+		$(".nav li").find(".active").removeClass("active");
+		$(this).parent().addClass("active");
+	});
+});
